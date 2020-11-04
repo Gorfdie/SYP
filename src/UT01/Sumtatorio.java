30342a13e1416c654531c7e068d2582fd662abbd
Diego Rodríguez <esferca13@gmail.com>
2020-11-04 19:54:43 +0000

    Add number sumatory range

--- src/UT01/Sumtatorio.java
+++ src/UT01/Sumtatorio.java
@@ -2,6 +2,12 @@ package UT01;
 
 public class Sumtatorio {
     public int sumar(int num1, int num2) {
-        return num1 + num2;
+        int minimo = Math.min(num1, num2);
+        int maximo = Math.max(num1, num2);
+        int suma = 0;
+        for (int i = minimo; i <= maximo; i++) {
+            suma += i;
+        }
+        return suma;
     }
 }
